0b8489d0f44b268516d60efacacd59bb4de8c082
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-08-15 09:16:08 -0400

    Added message module.

---
+++ pkg/module/message/message.go
@@ -0,0 +1,46 @@
+// Copyright (c) Forge
+// SPDX-License-Identifier: MPL-2.0
+
+package message
+
+import (
+	"context"
+
+	"github.com/trippsoft/forge/pkg/hclspec"
+	"github.com/trippsoft/forge/pkg/module"
+	"github.com/zclconf/go-cty/cty"
+)
+
+var (
+	inputSpec = hclspec.NewSpec(hclspec.Object(map[string]*hclspec.ObjectField{
+		"message": hclspec.RequiredField(hclspec.String),
+	}))
+
+	_ module.Module = (*Module)(nil)
+)
+
+type Module struct{}
+
+// InputSpec implements module.Module.
+func (s *Module) InputSpec() *hclspec.Spec {
+	return inputSpec
+}
+
+// Validate implements module.Module.
+func (s *Module) Validate(config *module.RunConfig) error {
+	return nil // No specific validation needed for this module.
+}
+
+// Run implements module.Module.
+func (s *Module) Run(ctx context.Context, config *module.RunConfig) *module.Result {
+
+	message := config.Input["message"].AsString()
+
+	output := map[string]cty.Value{}
+
+	result := module.NewSuccess(true, output)
+
+	result.Message = message
+
+	return result
+}
---
+++ pkg/module/message/message_test.go
@@ -0,0 +1,103 @@
+// Copyright (c) Forge
+// SPDX-License-Identifier: MPL-2.0
+
+package message
+
+import (
+	"context"
+	"testing"
+
+	"github.com/trippsoft/forge/pkg/inventory"
+	"github.com/trippsoft/forge/pkg/module"
+	"github.com/trippsoft/forge/pkg/transport"
+	"github.com/zclconf/go-cty/cty"
+)
+
+func TestModuleInputSpec(t *testing.T) {
+
+	module := &Module{}
+
+	spec := module.InputSpec()
+	if spec == nil {
+		t.Fatal("Expected non-nil input spec")
+	}
+
+	errs := spec.ValidateSpec()
+	if len(errs) > 0 {
+		t.Errorf("expected no errors from ValidateSpec(), got %d errors", len(errs))
+		for _, err := range errs {
+			t.Errorf("expected no errors from ValidateSpec(), got: %v", err)
+		}
+	}
+}
+
+func TestModuleValidate(t *testing.T) {
+
+	m := &Module{}
+
+	input := map[string]cty.Value{
+		"message": cty.StringVal("Hello, World!"),
+	}
+
+	mockTransport := transport.NewMockTransport()
+	escalateConfig := inventory.NewEscalateConfig("")
+	host := inventory.NewHost("linux", mockTransport, escalateConfig, map[string]cty.Value{})
+
+	config := &module.RunConfig{
+		Transport:  mockTransport,
+		HostInfo:   host.Info(),
+		Escalation: nil,
+		Input:      input,
+	}
+
+	err := m.Validate(config)
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+}
+
+func TestModuleRun(t *testing.T) {
+
+	expectedMessage := "Hello, World!"
+
+	mockTransport := transport.NewMockTransport()
+
+	escalateConfig := inventory.NewEscalateConfig("")
+
+	host := inventory.NewHost("linux", mockTransport, escalateConfig, map[string]cty.Value{})
+
+	p := &Module{}
+
+	input := map[string]cty.Value{
+		"message": cty.StringVal("Hello, World!"),
+	}
+
+	config := &module.RunConfig{
+		Transport:  mockTransport,
+		HostInfo:   host.Info(),
+		Escalation: nil,
+		Input:      input,
+	}
+
+	result := p.Run(context.Background(), config)
+
+	if result.Err != nil {
+		t.Fatalf("Expected no error, got: %v", result.Err)
+	}
+
+	if !result.Changed {
+		t.Fatal("Expected module to indicate changes were made")
+	}
+
+	if result.Output == nil {
+		t.Fatal("Expected non-nil output")
+	}
+
+	if result.Message != expectedMessage {
+		t.Fatalf("Expected message %q, got %q", expectedMessage, result.Message)
+	}
+
+	if len(result.Output) != 0 {
+		t.Fatalf("Expected output to have 0 keys, got: %d", len(result.Output))
+	}
+}
