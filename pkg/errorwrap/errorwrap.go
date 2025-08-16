c926e1cd9fc6b048674a677f155909d0a21f050a
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-08-16 18:22:08 -0400

    Added error unwrapping helper function.

---
+++ pkg/errorwrap/errorwrap.go
@@ -0,0 +1,29 @@
+package errorwrap
+
+// UnwrapErrors unwraps any error messages recursively that were joined using errors.Join(...)
+func UnwrapErrors(e error) []error {
+	if e == nil {
+		return nil
+	}
+
+	wrapped, ok := e.(interface{ Unwrap() []error })
+	if !ok {
+		return []error{e}
+	}
+
+	var errs []error
+	for _, err := range wrapped.Unwrap() {
+		if err == nil {
+			continue
+		}
+
+		if errs == nil {
+			errs = UnwrapErrors(err)
+			continue
+		}
+
+		errs = append(errs, UnwrapErrors(err)...)
+	}
+
+	return errs
+}
