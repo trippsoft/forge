bd5eee1c5d2eec2cdc389595982a5585ad28df73
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-11-19 08:32:55 -0500

    Added entrypoint and core package docs.

---
+++ main.go
@@ -0,0 +1,8 @@
+// Copyright (c) Forge
+// SPDX-License-Identifier: MPL-2.0
+
+// Package main is the entry point for the Forge CLI application.
+package main
+
+func main() {
+}
---
+++ pkg/core/doc.go
@@ -0,0 +1,7 @@
+// Copyright (c) Forge
+// SPDX-License-Identifier: MPL-2.0
+
+// Package core contains the core types and interfaces for Forge.
+//
+// This will include any types that will be used by two or more of the CLI tool, Packer plugin, and/or web interfaces.
+package core
