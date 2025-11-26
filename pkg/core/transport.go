a815bfea1eea3e441e1ebc1c42a41d9789958bdb
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-11-26 10:47:45 -0500

    Added temporary Transport interface with functions for returning the type, connecting, and closing the connection.

---
+++ pkg/core/transport.go
@@ -0,0 +1,20 @@
+// Copyright (c) Forge
+// SPDX-License-Identifier: MPL-2.0
+
+package core
+
+// TransportType represents the type of transport used for connecting to managed systems.
+type TransportType string
+
+const (
+	TransportTypeLocal TransportType = "local"
+	TransportTypeSSH   TransportType = "ssh"
+)
+
+// Transport defines the transport mechanism for interacting a managed system.
+type Transport interface {
+	Type() TransportType // Type returns the type of transport.
+
+	Connect() error // Connect establishes the transport connection.
+	Close() error   // Close terminates the transport connection.
+}
