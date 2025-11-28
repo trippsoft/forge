28fdcb394079653352419aef0e89011907a19077
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-11-27 19:10:16 -0500

    Added module registry.

--- pkg/module/module.go
+++ pkg/module/module.go
@@ -3,6 +3,57 @@
 
 package module
 
+import (
+	"fmt"
+	"maps"
+)
+
 // Module abstracts local and plugin modules.
 type Module interface {
+}
+
+// Registry manages a collection of modules.
+//
+// It allows for registering new modules and looking them up by name.
+type Registry struct {
+	modules map[string]Module
+}
+
+// Register adds a new module to the registry.
+func (r *Registry) Register(name string, module Module) error {
+	if r.modules == nil {
+		r.modules = make(map[string]Module)
+	}
+
+	if _, exists := r.modules[name]; exists {
+		return fmt.Errorf("module %q is already registered", name)
+	}
+
+	r.modules[name] = module
+	return nil
+}
+
+// Lookup retrieves a module by its name from the registry.
+func (r *Registry) Lookup(name string) (Module, bool) {
+	module, exists := r.modules[name]
+
+	return module, exists
+}
+
+// Modules returns a copy of the registered modules in the registry.
+//
+// This is used mostly for testing purposes to avoid modifying the original map.
+func (r *Registry) Modules() map[string]Module {
+	if r.modules == nil {
+		return make(map[string]Module)
+	}
+
+	return maps.Clone(r.modules)
+}
+
+// NewRegistry creates a new module registry.
+func NewRegistry() *Registry {
+	return &Registry{
+		modules: make(map[string]Module),
+	}
 }
