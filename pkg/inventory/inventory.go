e65233377fa9c756e8ed5d457a1f6ad87b39f5ee
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-12-01 12:10:23 -0500

    Added inventory methods for dealing with evaluation contexts.

--- pkg/inventory/inventory.go
+++ pkg/inventory/inventory.go
@@ -38,6 +38,9 @@ type Host struct {
 
 	info *info.HostInfo
 	vars map[string]cty.Value
+
+	stepContexts    []map[string]cty.Value
+	procedureInputs []map[string]cty.Value
 }
 
 // Name returns the name of the host.
@@ -50,9 +53,76 @@ func (h *Host) Transport() transport.Transport {
 	return h.transport
 }
 
+// EscalateConfig returns the escalation configuration for the host, if any.
+func (h *Host) EscalateConfig() *EscalateConfig {
+	return h.escalateConfig
+}
+
+// Info returns the host's information as a HostInfo instance.
+func (h *Host) Info() *info.HostInfo {
+	return h.info
+}
+
 // Vars returns the variables associated with the host.
 func (h *Host) Vars() map[string]cty.Value {
 	return h.vars
+}
+
+// GetCurrentContextSteps retrieves the current step context for the host.
+func (h *Host) GetCurrentContextSteps() (map[string]cty.Value, error) {
+	if len(h.stepContexts) == 0 {
+		return nil, errors.New("no step context available")
+	}
+
+	return h.stepContexts[len(h.stepContexts)-1], nil
+}
+
+// StoreStepOutput stores a step in the current step context.
+//
+// If the key already exists, it will overwrite the existing value.
+// This is by design to allow for step updates.
+func (h *Host) StoreStepOutput(key string, value cty.Value) error {
+	stepContext, err := h.GetCurrentContextSteps()
+	if err != nil {
+		return err
+	}
+
+	stepContext[key] = value // Overwrites existing keys by design
+	return nil
+}
+
+// StartProcedure initializes a new procedure context for the host.
+func (h *Host) StartProcedure(inputs map[string]cty.Value) {
+	h.stepContexts = append(h.stepContexts, make(map[string]cty.Value))
+	h.procedureInputs = append(h.procedureInputs, inputs)
+}
+
+// EndProcedure ends the current procedure context for the host.
+func (h *Host) EndProcedure() error {
+	if len(h.stepContexts) < 2 {
+		return errors.New("no step context to end")
+	}
+	h.stepContexts = h.stepContexts[:len(h.stepContexts)-1]
+	if len(h.procedureInputs) < 1 {
+		return errors.New("no procedure inputs to end")
+	}
+	h.procedureInputs = h.procedureInputs[:len(h.procedureInputs)-1]
+	return nil
+}
+
+// ClearSteps clears all steps and procedure inputs for the host.
+func (i *Host) ClearSteps() {
+	i.stepContexts = []map[string]cty.Value{make(map[string]cty.Value)}
+	i.procedureInputs = []map[string]cty.Value{}
+}
+
+// GetCurrentProcedureInputs retrieves the inputs for the current procedure context.
+func (h *Host) GetCurrentProcedureInputs() map[string]cty.Value {
+	if len(h.procedureInputs) == 0 {
+		return nil
+	}
+
+	return h.procedureInputs[len(h.procedureInputs)-1]
 }
 
 // HostBuilder is used to build Host instances.
@@ -179,4 +249,11 @@ func (i *Inventory) Target(name string) ([]*Host, bool) {
 func (i *Inventory) Targets() map[string][]*Host {
 	targets := maps.Clone(i.targets)
 	return targets
+}
+
+// ClearSteps clears all steps and procedure inputs for the inventory.
+func (i *Inventory) ClearSteps() {
+	for _, host := range i.hosts {
+		host.ClearSteps()
+	}
 }
